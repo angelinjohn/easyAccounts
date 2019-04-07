d45b58600ebffb501e2057526763b6958319c221
Angelin John <angelinmaryjohn@gmail.com>
2019-04-08 02:33:11 +0530

    Transaction related APIs

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def users():
 		cursor.close() 
 		conn.close()
 		
-@app.route('/user/')
+@app.route('/user/<id>')
 def user(id):
 	try:
 		conn = mysql.connect()
@@ -140,6 +140,125 @@ def update_user():
 		cursor.close() 
 		conn.close()
 		
+#All transaction related APIs will follow
+
+#Add Transaction
+@app.route('/<userid>/transaction', methods=['POST'])
+@cross_origin()
+def add_transaction(userid):
+	try:
+		_json = request.json
+		_txnType = _json['txnType']
+		_category = _json['category']
+		_amt= _json['amt']
+		_date=_json['date']
+
+		if _txnType and _category and _date and request.method == 'POST':
+			if not _amt:
+				_amt=0.00
+			
+			# Query to add transaction
+			sql = "INSERT INTO tbl_transaction(user_id, txntype, category,amt,date) VALUES(%s, %s, %s,%s,%s)"
+			data = (userid,_txnType,_category,_amt,_date)
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.execute(sql, data)
+			conn.commit()
+			print("Last inserted record has id ", cursor.lastrowid())
+			resp = jsonify('Transaction added successfully!')
+			resp.status_code = 200
+			return resp
+			#return get_transaction(userid,cursor.lastrowid())
+		else:
+			return not_found()
+	except Exception as e:
+		print(e)
+
+# Get Transaction
+@app.route('/<userid>/transaction/<txnId>', methods=['GET'])
+@cross_origin()
+def get_transaction(userid,txnId):
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT * FROM tbl_user WHERE user_id=%s AND txn_id=%s", id)
+		row = cursor.fetchone()
+		resp = jsonify(row)
+		resp.status_code = 200
+		return resp
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+#Update transaction
+@app.route('/<userid>/transaction/<txnId>', methods=['PUT'])
+@cross_origin()
+def update_txn(userid,txnId):
+	try:
+		_json = request.json
+		_txnType = _json['txnType']
+		_category = _json['category']
+		_amt= _json['amt']
+		_date=_json['date']	
+		# validate the received values
+		if userid and txnId and _txnType and _category and _date and request.method == 'PUT':
+			
+			sql = "UPDATE tbl_transaction SET txntype=%s, category=%s, amt=%s,date=%s WHERE user_id=%s and txn_id=%s"
+			data = (_txnType,_category,_amt,_date,userid,txnId)
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.execute(sql, data)
+			conn.commit()
+			resp = jsonify('Transaction updated successfully!')
+			resp.status_code = 200
+			return resp
+			#return get_transaction(userid,txnId)
+		else:
+			return not_found()
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+#Delete Transaction
+@app.route('/<userid>/transaction/<txnId>',methods=['DELETE'])
+def delete_txn(userid,txnId):
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor()
+		data=(userid,txnId)
+		cursor.execute("DELETE FROM tbl_transaction WHERE user_id=%s AND txn_id=%s",data)
+		conn.commit()
+		resp = jsonify('Transaction deleted successfully!')
+		resp.status_code = 204
+		return resp
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+#Fetch All Transactions
+@app.route('<userid>/transactions')
+@cross_origin()
+def transactions(userid):
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT * FROM tbl_transaction where user_id=%s",(userid))
+		rows = cursor.fetchall()
+
+		resp = jsonify(rows)
+		print(rows)
+		resp.status_code = 200
+		return resp
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()	
 
 		
 @app.errorhandler(404)
