f4bda907cb00dde8bda63ec3025c902b033ab5e0
Angelin John <angelinmaryjohn@gmail.com>
2019-04-08 00:47:25 +0530

    07/04/2018 changes

--- easy_account_sui/src/Login.js
+++ easy_account_sui/src/Login.js
@@ -5,67 +5,67 @@ import RaisedButton from 'material-ui/RaisedButton';
 import axios from 'axios';
 import TextField from 'material-ui/TextField';
 import UploadScreen from './UploadScreen';
-class Login extends Component{
-    constructor(props){
+class Login extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            userName:'',
-            passWord:''
+        this.state = {
+            email: '',
+            password: ''
         }
     }
-    handleClick(event){
-        var apiBaseUrl = "http://localhost:5000/api/";
+    handleClick(event) {
+        var apiBaseUrl = "http://localhost:5000/";
         var self = this;
-        var payload={
-        "email":this.state.username,
-        "password":this.state.password
+        var payload = {
+            "email": this.state.email,
+            "password": this.state.password
         }
-        axios.post(apiBaseUrl+'login', payload)
-        .then(function (response) {
-        console.log(response);
-        if(response.data.code == 200){
-        console.log("Login successfull");
-        var uploadScreen=[];
-        uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
-        self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
-        }
-        else if(response.data.code == 204){
-        console.log("Username password do not match");
-        alert("username password do not match")
-        }
-        else{
-        console.log("Username does not exists");
-        alert("Username does not exist");
-        }
-        })
-        .catch(function (error) {
-        console.log(error);
-        });
-        }
-    render(){
-        return(<div>
+        axios.post(apiBaseUrl + 'login', payload)
+            .then(function (response) {
+                console.log(response);
+                if (response.status == 200) {
+                    console.log("Login successfull");
+                    var uploadScreen = [];
+                    uploadScreen.push(<UploadScreen appContext={self.props.appContext} />)
+                    self.props.appContext.setState({ loginPage: [], uploadScreen: uploadScreen })
+                }
+                else if (response.status == 204) {
+                    console.log("Username password do not match");
+                    alert("username password do not match")
+                }
+                else {
+                    console.log("Username does not exists");
+                    alert("Username does not exist");
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+    render() {
+        return (<div>
             <MuiThemeProvider>
                 <div>
-                    <AppBar title="EasyAccounts"/>
+                    <AppBar title="EasyAccounts" />
                     <TextField hintText="Enter your Username"
-                            floatingLabelText="Username"
-                            onChange={(event,newValue)=>this.setState({userName:newValue})}/>
-                    <br/>
+                        floatingLabelText="Email"
+                        onChange={(event, newValue) => this.setState({ email: newValue })} />
+                    <br />
                     <TextField hintText="Enter password"
-                               floatingLabelText="Password"
-                               onChange={(event,newValue)=>this.setState({passWord:newValue})}/>
-                    <br/>
+                        floatingLabelText="Password"
+                        onChange={(event, newValue) => this.setState({ password: newValue })} />
+                    <br />
                     <RaisedButton label="Submit"
-                                primary={true}
-                                style={style}
-                                onClick={(event)=>this.handleClick(event)}/>
+                        primary={true}
+                        style={style}
+                        onClick={(event) => this.handleClick(event)} />
                 </div>
             </MuiThemeProvider>
         </div>)
     }
 
 }
-const style={
-    margin:15,
+const style = {
+    margin: 15,
 }
 export default Login;
--- main.py
+++ main.py
@@ -37,16 +37,55 @@ def add_user():
 	finally:
 		cursor.close() 
 		conn.close()
-		
+
+@app.route('/login', methods=['POST'])
+@cross_origin()
+def login_user():
+	try:
+		_json = request.json
+		_email = _json['email']
+		_password = _json['password']
+		# validate the received values
+		if _email and _password and request.method == 'POST':
+			#do not save password as a plain text
+			_hashed_password = generate_password_hash(_password)
+			# save edits
+			sql = """SELECT * FROM tbl_user WHERE user_email=%s"""
+			data = (_email)
+			conn = mysql.connect()
+			cursor = conn.cursor(pymysql.cursors.DictCursor)
+			cursor.execute(sql, data)
+			rv = cursor.fetchall()
+			resp = jsonify('Login !')
+			
+			if rv:
+
+				if check_password_hash(rv[1]["user_password"],_password):
+					resp = jsonify('Login successful!')
+					resp.status_code = 200
+				else:
+					resp = jsonify('Unauthorized Login')
+					resp.status_code = 204
+			else:
+				resp = jsonify('Username does not exist')
+				resp.status_code = 210
+			return resp
+		else:
+			return not_found()
+	except Exception as e:
+		print(e)
+				
 @app.route('/users')
 @cross_origin()
 def users():
 	try:
 		conn = mysql.connect()
 		cursor = conn.cursor(pymysql.cursors.DictCursor)
-		cursor.execute("SELECT * FROM tbl_user")
+		cursor.execute("SELECT * FROM tbl_user where user_email=%s",('angelin@gmail.com',))
 		rows = cursor.fetchall()
+
 		resp = jsonify(rows)
+		print(rows)
 		resp.status_code = 200
 		return resp
 	except Exception as e:
@@ -101,21 +140,7 @@ def update_user():
 		cursor.close() 
 		conn.close()
 		
-@app.route('/delete/')
-def delete_user(id):
-	try:
-		conn = mysql.connect()
-		cursor = conn.cursor()
-		cursor.execute("DELETE FROM tbl_user WHERE user_id=%s", (id,))
-		conn.commit()
-		resp = jsonify('User deleted successfully!')
-		resp.status_code = 200
-		return resp
-	except Exception as e:
-		print(e)
-	finally:
-		cursor.close() 
-		conn.close()
+
 		
 @app.errorhandler(404)
 def not_found(error=None):
